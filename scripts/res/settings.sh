ab63911a630b3034b3ff20f526bfc302cbedb7fc
Oleksii <grossqx@gmail.com>
2025-10-07 16:14:21 +0200

    client settings file

---
+++ scripts/res/settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+script_version="0.1.12"
+
+# Encryption
+key_derivation="-pbkdf2"
+crypto_cipher="-aes-256-cbc"
+crypto_key="seed"
+
+# Array of public DNS servers to test for connectivity.
+declare -a test_hosts=("isc.org" "google.com" "cloudflare.com" "1.1.1.1" "8.8.8.8")
+ping_timeout=10
