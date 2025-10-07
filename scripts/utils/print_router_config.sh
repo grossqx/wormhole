768edde0780a7ce401798c7c1279b1f86504c447
Oleksii <grossqx@gmail.com>
2025-10-07 12:07:36 +0200

    display router configuration

---
+++ scripts/utils/print_router_config.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CONFIG_PATH=$1
+DEVICE_MAC=$2
+DEVICE_INTERFACE=$3
+PRINT_TO_FILE=$4
+
+if [[ "$PRINT_TO_FILE" != "true" ]]; then
+    source ${base_dir}/res/theme.env
+else
+    T_BOLD=""
+    T_NC=""
+    T_ITALIC=""
+    T_BLUE=""
+    T_YELLOW=""
+fi
+
+source "${CONFIG_PATH}"
+
+if [[ -z $RPI_IP_ADDR ]]; then
+    display_ip="< IP address picked by you >"
+else
+    display_ip=$RPI_IP_ADDR
+fi
+
+echo -e  "==============================================================================="
+echo -e "${T_BOLD}Router configuration${T_NC} - ${T_ITALIC}these have to be configured manually${T_NC}"
+echo -e "${T_BLUE}1. DHCP - static IP address:${T_NC}"
+echo -e "\thostname: ${T_BLUE}${RPI_HOSTNAME}${T_NC}"
+echo -e "\tinternal IP address: ${T_BLUE}${display_ip}${T_NC}" 
+if [[ ! -z "$DEVICE_MAC" ]]; then
+    echo -e "\tMAC: ${T_BLUE}${DEVICE_MAC}${T_NC} (${DEVICE_INTERFACE})"
+fi
+echo -e "${T_BLUE}2. Port forwarding rules:${T_NC}"
+echo -e "\tWireguard VPN: port ${T_BLUE}${RPI_WH_PORT}${T_NC} at ${T_BLUE}${display_ip}${T_NC} ${T_BLUE}(UDP protocol)${T_NC}" 
+if [[ ! -z $RPI_DOMAIN ]]; then
+    echo -e "${T_BLUE}3. DNS record ${T_NC}"
+    echo -e "\tsource: ${T_BLUE}${RPI_DOMAIN}${T_NC}" 
+    echo -e "\tdestination: ${T_BLUE}http://${display_ip}:${RPI_WH_PORT}${T_NC}" 
+else
+    echo -e "${T_YELLOW}3. DNS record not configured${T_NC}"
+fi
+echo -e  "==============================================================================="
+exit 0
